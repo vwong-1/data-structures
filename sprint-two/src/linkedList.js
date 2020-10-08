7c52bd4bec8fe7157c3ce64d0dd18e2e143940ec
Victor Wong <victorw1992@gmail.com>
2020-10-07 23:33:23 -0700

    Psuedocode linkList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,23 +3,36 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  let count = 0;
   list.addToTail = function(value) {
+    //initiate first value as head and tail
+    //any new Node values added should equal current tail
+    //increment count
+
   };
 
   list.removeHead = function() {
+    //initiate result
+    //set head equal to next node
+    //return result
   };
 
   list.contains = function(target) {
+    //iterating through list
+      //check if target is there
+      //returns true if there
+    //returns false if true is not
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function(value, count) {
   var node = {};
 
   node.value = value;
   node.next = null;
+  // node.count = count;
 
   return node;
 };
@@ -27,3 +40,8 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+
+   // { head : {1, 1}  {3: {2, 2} {tail: {3, null}}}
+  // { head : {1, 3}  {3, 2} {tail: {2, null}}}
