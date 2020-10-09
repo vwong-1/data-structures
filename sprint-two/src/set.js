3b6316171c062f5f0214954d89c3a1ca4a6e86c5
Simei Yeh <simei.yeh@gmail.com>
2020-10-08 23:39:38 -0700

    Implemented set.js. Possible refactor optional advanced later

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,14 +7,22 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this[item] = 1;
 };
 
 setPrototype.contains = function(item) {
+  return (this[item]) ? true : false;
 };
 
 setPrototype.remove = function(item) {
+  delete this[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ *
+ * add       O(1)
+ * contains  O(1)
+ * remove    O(1)
+ *
  */
