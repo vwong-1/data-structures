82f29e2841606050c6518e11cdfcd15d032c2fef
Simei Yeh <simei.yeh@gmail.com>
2020-10-08 17:04:28 -0700

    tree implementation finished

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,21 +3,40 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  let result = false;
+  let currentTree = this;
+
+  var findTarget = function(target, node) {
+    if (node.value === target) {
+      return result = true;
+    } else if (node.children.length) {
+      for (let i = 0; i < node.children.length; i++) {
+        findTarget(target, node.children[i]);
+      }
+    }
+    return result;
+  }
+
+  return findTarget(target, currentTree);
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addChild:   O(1)
+ * contains:   O(n)
  */
