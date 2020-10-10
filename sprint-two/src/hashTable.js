425a44a5ad5e8211156df4c0f67c39779e64a8a3
Simei Yeh <simei.yeh@gmail.com>
2020-10-09 19:08:15 -0700

    Updated with refactor

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -9,8 +9,9 @@ HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
   // if something exists, check keys to see if already matching key
-  if (this._storage.get(index)) {
+  if (this.retrieve(k)) {
   // if matching key, replace
+    //loop through current index
   // if not, link to index
   } else {
     // if nothing exists in index, set
@@ -20,12 +21,19 @@ HashTable.prototype.insert = function(k, v) {
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-
+  // [ , , ,]
+  let indexList = this._storage.get(index);
   //check if something exists
+  if (indexList) {
   //if something exists, loop through the index to find the key
-
+    for (let i = 0; i < indexList.length; i++) {
+      if (indexList[i][0] === k) {
+        return indexList[i][1];
+      }
+    }
+  }
   //if index is empty, return undefined else return value
-  return this._storage.get(index)[1]; // Can we use braket notation to access the value?
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
@@ -46,6 +54,21 @@ HashTable.prototype.remove = function(k) {
  * retrieve    best case O(1) worst case O(n)
  * remove      best case O(1) worst case O(n)
  *
+ */
+
+
+ */
+
+
+ */
+
+
+ */
+
+
+ */
+
+
  */
 
 
