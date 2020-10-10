b54e0c79fe399fc5085b1cc88868f17a7642736d
Victor Wong <victorw1992@gmail.com>
2020-10-09 19:39:08 -0700

    hashTable updates -- need to finish implementing prototypes

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,24 +8,42 @@ var HashTable = function() {
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
+  let indexList = this._storage.get(index);
   // if something exists, check keys to see if already matching key
   if (this._storage.get(index)) {
   // if matching key, replace
+    for (let i = 0; i < indexList.length; i++) {
+      if (indexList[i][0] === k) {
+        indexList[i][1] = v;
+      }
+    }
   // if not, link to index
   } else {
     // if nothing exists in index, set
-    this._storage.set(index, [k, v]);
+    this._storage.set(index, [[k, v]]);
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
+  let indexList = this._storage.get(index);
+  console.log(indexList);
+  //check if something exists
+  if (indexList) {
+  //if something exists, loop through the index to find the key
+    for (let i = 0; i < indexList.length; i++) {
+      if (indexList[i][0] === k) {
+        return indexList[i][1];
+      }
+    }
+  }
+
   //check if something exists
   //if something exists, loop through the index to find the key
 
   //if index is empty, return undefined else return value
-  return this._storage.get(index)[1]; // Can we use braket notation to access the value?
+  return undefined; // Can we use braket notation to access the value?
 };
 
 HashTable.prototype.remove = function(k) {
@@ -39,13 +57,13 @@ HashTable.prototype.remove = function(k) {
 };
 
 
- /*
- * Complexity: What is the time complexity of the above functions?
- *
- * insert      best case O(1) worst case O(n)
- * retrieve    best case O(1) worst case O(n)
- * remove      best case O(1) worst case O(n)
- *
- */
+/*
+* Complexity: What is the time complexity of the above functions?
+*
+* insert      best case O(1) worst case O(n)
+* retrieve    best case O(1) worst case O(n)
+* remove      best case O(1) worst case O(n)
+*
+*/
 
 
